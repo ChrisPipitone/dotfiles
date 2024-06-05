6700f7d7558f2411e98b4f3103ff11d049608aed
chris.pipitone <chris.pipitone>
2024-06-05 17:37:40 -0400

    nvim dresssing

--- nvim/lua/chris/plugins/dressing.lua
+++ nvim/lua/chris/plugins/dressing.lua
@@ -1,4 +1,12 @@
 return {
 	"stevearc/dressing.nvim",
 	event = "VeryLazy",
+	opts = {
+		input = {
+			enabled = true,
+			title_pos = "center",
+			border = "rounded",
+			relative = "cursor",
+		},
+	},
 }
