db7cd067b76fc60a31326a77588b4964e21c1ae5
Chris Pipitone <chrispipitone@protonmail.com>
2026-08-30 18:26:10 -0400

    feat(lsp): add clangd flags and wire clangtidy to nvim-lint

--- common/.config/nvim/lua/chris/plugins/linting.lua
+++ common/.config/nvim/lua/chris/plugins/linting.lua
@@ -11,6 +11,8 @@ return {
 			typescriptreact = { "eslint_d" },
 			svelte = { "eslint_d" },
 			python = { "ruff" },
+			c = { "clangtidy" },
+			cpp = { "clangtidy" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
--- common/.config/nvim/lua/chris/plugins/lsp/lspconfig.lua
+++ common/.config/nvim/lua/chris/plugins/lsp/lspconfig.lua
@@ -104,6 +104,15 @@ return {
     })
 
     vim.lsp.config("clangd", {
+      cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+        "--fallback-style=llvm",
+      },
       filetypes = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
     })
 
