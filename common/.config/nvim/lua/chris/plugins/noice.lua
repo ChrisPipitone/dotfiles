1dca894533900b85b76bb902b0ff935fedb89a4c
Chris Pipitone <chrispipitone@protonmail.com>
2026-08-30 18:17:06 -0400

    feat(noice): enable LSP signature help

--- common/.config/nvim/lua/chris/plugins/noice.lua
+++ common/.config/nvim/lua/chris/plugins/noice.lua
@@ -4,6 +4,7 @@ return {
   dependencies = { "MunifTanjim/nui.nvim" },
   opts = {
     lsp = {
+      signature = { enabled = true },
       override = {
         ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
         ["vim.lsp.util.stylize_markdown"] = true,
